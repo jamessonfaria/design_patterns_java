c851f941724b1027db93e959d0542057b9471cb3
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-02 16:14:06 -0300

    Adjust imports

--- src/main/java/br/com/jamesson/estructural_patterns/composite/folders/Client.java
+++ src/main/java/br/com/jamesson/estructural_patterns/composite/folders/Client.java
@@ -1,30 +1,30 @@
 package br.com.jamesson.estructural_patterns.composite.folders;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import br.com.jamesson.estructural_patterns.composite.folders.model.File;
 import br.com.jamesson.estructural_patterns.composite.folders.model.FileSystemItem;
 import br.com.jamesson.estructural_patterns.composite.folders.model.Folder;
 
 public class Client {
 
-	public static FileSystemItem createCompositeFromFile(File file) {
-		if(!file.isDirectory()) return new br.com.jamesson.estructural_patterns.composite.folders.model.File(file.getName());
+	public static FileSystemItem createCompositeFromFile(java.io.File file) {
+		if(!file.isDirectory()) return new File(file.getName());
 		List<FileSystemItem> childs = new ArrayList<>();
-		File[] files = file.listFiles();
-		for(File javaFile: files) {
+		java.io.File[] files = file.listFiles();
+		for(java.io.File javaFile: files) {
 			childs.add(createCompositeFromFile(javaFile));
 		}
 		return new Folder(file.getName(), childs);
 	}
 	
 	public static void main(String[] args) {
-		FileSystemItem file1 = new br.com.jamesson.estructural_patterns.composite.folders.model.File("File1.txt");
-		FileSystemItem file2 = new br.com.jamesson.estructural_patterns.composite.folders.model.File("File2.txt");
-		FileSystemItem file3 = new br.com.jamesson.estructural_patterns.composite.folders.model.File("File3.txt");
-		FileSystemItem file4 = new br.com.jamesson.estructural_patterns.composite.folders.model.File("File4.txt");
+		FileSystemItem file1 = new File("File1.txt");
+		FileSystemItem file2 = new File("File2.txt");
+		FileSystemItem file3 = new File("File3.txt");
+		FileSystemItem file4 = new File("File4.txt");
 		FileSystemItem sf1 = new Folder("sf1", Arrays.asList(file1));
 		FileSystemItem sf2 = new Folder("sf2", Arrays.asList(file2, sf1));
 		FileSystemItem folder = new Folder("folder", Arrays.asList(sf2, file3, file4));
@@ -34,7 +34,7 @@ public class Client {
 		System.out.println("------------");
 		
 		String dir = System.getProperty("user.dir");
-		FileSystemItem fromDisk = createCompositeFromFile(new File(dir + File.separator + "src"));
+		FileSystemItem fromDisk = createCompositeFromFile(new java.io.File(dir + java.io.File.separator + "src"));
 		fromDisk.print(">");
 	}
 }
