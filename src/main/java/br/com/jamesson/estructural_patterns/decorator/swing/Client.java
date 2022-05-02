2e2792c45fae96b0b5dd876af784e469bee02e72
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-02 17:08:07 -0300

    Adjust

--- src/main/java/br/com/jamesson/estructural_patterns/decorator/swing/Client.java
+++ src/main/java/br/com/jamesson/estructural_patterns/decorator/swing/Client.java
@@ -11,9 +11,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		JFrame frame =new JFrame();  
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new FlowLayout());
-		frame.setSize(400,200);  
 		
 //		JTextArea textArea = new JTextArea(10, 30);
 //		frame.add(textArea);
@@ -22,8 +20,11 @@ public class Client {
 		JScrollPane scroll = new JScrollPane(textArea, 
 				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
 		frame.add(scroll);
-		          
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,200);            
 		frame.setVisible(true);
 	}
 }
