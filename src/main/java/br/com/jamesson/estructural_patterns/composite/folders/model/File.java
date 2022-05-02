676de759ed9a84dbb9c29b844866e2c991f1b324
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-02 14:58:49 -0300

    Implement pattern composite with sample folder

---
+++ src/main/java/br/com/jamesson/estructural_patterns/composite/folders/model/File.java
@@ -0,0 +1,14 @@
+package br.com.jamesson.estructural_patterns.composite.folders.model;
+
+public class File implements FileSystemItem {
+	private String name;
+	
+	public File(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void print(String structure) {
+		System.out.println(structure + name);
+	}
+}
---
+++ src/main/java/br/com/jamesson/estructural_patterns/composite/folders/model/FileSystemItem.java
@@ -0,0 +1,5 @@
+package br.com.jamesson.estructural_patterns.composite.folders.model;
+
+public interface FileSystemItem {
+	void print(String structure);
+}
