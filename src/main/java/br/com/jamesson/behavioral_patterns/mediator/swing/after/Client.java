dfe6e989027b8e9a6967e75216ce231824073eb9
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-05 13:31:49 -0300

    Implement pattern mediator with swing sample

---
+++ src/main/java/br/com/jamesson/behavioral_patterns/mediator/swing/after/Client.java
@@ -0,0 +1,9 @@
+package br.com.jamesson.behavioral_patterns.mediator.swing.after;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		Screens screens = new Screens();
+		screens.init();
+	}
+}
