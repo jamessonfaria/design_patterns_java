55ebe91205504f9f367e84e84d30d4191de9d6c8
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-30 11:47:58 -0300

    Problem to use pattern state

---
+++ src/main/java/br/com/jamesson/behavioral_patterns/state/headphone/before/Client.java
@@ -0,0 +1,15 @@
+package br.com.jamesson.behavioral_patterns.state.headphone.before;
+
+public class Client {
+
+	public static void main(String[] args) {
+		HeadPhone phone = new HeadPhone();
+		phone.onClick();
+		phone.onLongClick();
+		phone.onClick();
+		phone.onClick();
+		phone.onClick();
+		phone.onLongClick();
+		phone.onClick();
+	}
+}
